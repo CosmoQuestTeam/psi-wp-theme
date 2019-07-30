5a5b3491f2007933e2f606bdffed2d69e80b898c
starstryder <starstryder@gmail.com>
2019-07-30 17:21:43 -0500

    updating footer to get plugins to load better

--- footer.php
+++ footer.php
@@ -23,3 +23,13 @@
     </div>
     <div style="clear: both;"></div>
 </div>
+
+<?php
+/* Always have wp_footer() just before the closing </body>
+ * tag of your theme, or you will break many plugins, which
+ * generally use this hook to reference JavaScript files.
+ */
+wp_footer();
+?>
+</body>
+</html>
