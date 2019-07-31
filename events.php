b32638077799e1af85263572d323e9de6cd5d262
starstryder <starstryder@gmail.com>
2019-07-30 19:10:39 -0500

    Adding an event page

---
+++ events.php
@@ -0,0 +1,25 @@
+<?php
+/* Template Name: Homepage */
+
+get_header();
+
+?>
+
+    <div class="container">
+        <div id="main" class="left">
+            <div id="full-width">
+
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <h2 class="post-title"><?php the_title(); ?></h2>
+                    <?php the_content(); ?>
+
+                <?php endwhile; else: ?>
+                    <?php _e('Sorry, no posts matched your criteria.'); ?>
+                <?php endif; ?>
+            </div><!--/post-->
+        </div>
+
+    </div>
+
+<?php
+get_footer();
